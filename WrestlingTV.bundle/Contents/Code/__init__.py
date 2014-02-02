5506233b2f193c5b9652f68b2aef7fbebfa35192
Chris Fairbanks <cfairbanks@etsy.com>
2014-02-02 12:14:15 -0500

    fix tvrage urls

--- WrestlingTV.bundle/Contents/Code/__init__.py
+++ WrestlingTV.bundle/Contents/Code/__init__.py
@@ -1,7 +1,7 @@
 TVRAGE_API_KEY = 'bX9ymM850oC0KCZ88YZn'
-TVRAGE_SEARCH_URL = 'http://services.tvrage.com/feeds/search.php?key=%s&show=%%s' % TVRAGE_API_KEY
-TVRAGE_SHOW_INFO_URL = 'http://services.tvrage.com/feeds/showinfo.php?key=%s&sid=%%s' % TVRAGE_API_KEY
-TVRAGE_EPISODE_LIST_URL = 'http://services.tvrage.com/feeds/episode_list.php?key=%s&sid=%%s' % TVRAGE_API_KEY
+TVRAGE_SEARCH_URL = 'http://services.tvrage.com/myfeeds/search.php?key=%s&show=%%s' % TVRAGE_API_KEY
+TVRAGE_SHOW_INFO_URL = 'http://services.tvrage.com/myfeeds/showinfo.php?key=%s&sid=%%s' % TVRAGE_API_KEY
+TVRAGE_EPISODE_LIST_URL = 'http://services.tvrage.com/myfeeds/episode_list.php?key=%s&sid=%%s' % TVRAGE_API_KEY
 
 TVDB_API_KEY = '607FE3FE16EDB11F'
 TVDB_BANNER_LIST_URL = 'http://thetvdb.com/api/%s/series/%%s/banners.xml' % TVDB_API_KEY
