fcd0c4fb187fb244647687d8da8dba40c9974ec7
Chris Fairbanks <cfairbanks@etsy.com>
2014-02-14 19:39:22 -0500

    use old API key until new one gets approved

--- WrestlingTV.bundle/Contents/Code/__init__.py
+++ WrestlingTV.bundle/Contents/Code/__init__.py
@@ -1,7 +1,8 @@
 from urllib2 import URLError
 import TVRageNetwork, ImageUpdater
 
-TVRAGE_API_KEY = 'bX9ymM850oC0KCZ88YZn'
+TVRAGE_API_KEY = 'P8q4BaUCuRJPYWys3RBV'
+# TVRAGE_API_KEY = 'bX9ymM850oC0KCZ88YZn'
 TVRAGE_SEARCH_URL = 'http://services.tvrage.com/myfeeds/search.php?key=%s&show=%%s' % TVRAGE_API_KEY
 TVRAGE_SHOW_INFO_URL = 'http://services.tvrage.com/myfeeds/showinfo.php?key=%s&sid=%%s' % TVRAGE_API_KEY
 TVRAGE_EPISODE_LIST_URL = 'http://services.tvrage.com/myfeeds/episode_list.php?key=%s&sid=%%s' % TVRAGE_API_KEY
